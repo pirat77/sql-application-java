a8a318f8f09a10f388fa96a768b59852450f68b9
Jasio <mcyanek@gmail.com>
2020-09-20 16:06:35 +0200

    implements script inserting applicants data into database

--- data.sql
+++ data.sql
@@ -63,3 +63,23 @@ VALUES ('Mateusz','Steliga','Mateusz','003630/123-5343','mateusz.steliga@codecoo
 INSERT INTO mentors (first_name, last_name, nick_name, phone_number, email, city, favourite_number)
 VALUES ('Attila','Molnár','Atesz','003670/630-0539','attila.molnar@codecool.com','Budapest',23
 );
+insert into applicants (first_name, last_name, phone_number, email, application_code) VALUES ('Dominique','Williams','003630/734-4926','dolor@laoreet.co.uk',61823
+);
+insert into applicants (first_name, last_name, phone_number, email, application_code) VALUES ('Jemima','Foreman','003620/834-6898','magna@etultrices.net',58324
+);
+insert into applicants (first_name, last_name, phone_number, email, application_code) VALUES ('Zeph','Massey','003630/216-5351','a.feugiat.tellus@montesnasceturridiculus.co.uk',61349
+);
+insert into applicants (first_name, last_name, phone_number, email, application_code) VALUES ('Joseph','Crawford','003670/923-2669','lacinia.mattis@arcu.co.uk',12916
+);
+insert into applicants (first_name, last_name, phone_number, email, application_code) VALUES ('Ifeoma','Bird','003630/465-8994','diam.duis.mi@orcitinciduntadipiscing.com',65603
+);
+insert into applicants (first_name, last_name, phone_number, email, application_code) VALUES ('Arsenio','Matthews','003620/804-1652','semper.pretium.neque@mauriseu.net',39220
+);
+insert into applicants (first_name, last_name, phone_number, email, application_code) VALUES ('Jemima','Cantu','003620/423-4261','et.risus.quisque@mollis.co.uk',10384
+);
+insert into applicants (first_name, last_name, phone_number, email, application_code) VALUES ('Carol','Arnold','003630/179-1827','dapibus.rutrum@litoratorquent.com',70730
+);
+insert into applicants (first_name, last_name, phone_number, email, application_code) VALUES ('Jane','Forbes','003670/653-5392','janiebaby@adipiscingenimmi.edu',56882
+);
+insert into applicants (first_name, last_name, phone_number, email, application_code) VALUES ('Ursa','William','003620/496-7064','malesuada@mauriseu.net',91220
+);
