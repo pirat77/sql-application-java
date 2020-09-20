dfa8f680990db475fc7183c3a128875ad7be624c
Jasio <mcyanek@gmail.com>
2020-09-20 17:34:08 +0200

    finishes implementation of all models

--- src/main/java/com/codecool/si3/model/Mentor.java
+++ src/main/java/com/codecool/si3/model/Mentor.java
@@ -4,33 +4,9 @@ public class Mentor {
     private int id;
     private String first_name;
     private String last_name;
+    private String nick_name;
     private String phone_number;
     private String email;
-    private int application_code;
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
-    public void setPhone_number(String phone_number) {
-        this.phone_number = phone_number;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setApplication_code(int application_code) {
-        this.application_code = application_code;
-    }
 
     public int getId() {
         return id;
@@ -44,6 +20,10 @@ public class Mentor {
         return last_name;
     }
 
+    public String getNick_name() {
+        return nick_name;
+    }
+
     public String getPhone_number() {
         return phone_number;
     }
@@ -52,7 +32,47 @@ public class Mentor {
         return email;
     }
 
-    public int getApplication_code() {
-        return application_code;
+    public String getCity() {
+        return city;
     }
+
+    public int getFavourite_number() {
+        return favourite_number;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setNick_name(String nick_name) {
+        this.nick_name = nick_name;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setFavourite_number(int favourite_number) {
+        this.favourite_number = favourite_number;
+    }
+
+    private String city;
+    private int favourite_number;
+
 }
