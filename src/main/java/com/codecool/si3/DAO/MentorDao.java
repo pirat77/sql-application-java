9023fdbe5afa56d53c4ce7e4f95125ee32b6069c
Jasio <mcyanek@gmail.com>
2020-09-20 18:10:00 +0200

    implements mentor dao

---
+++ src/main/java/com/codecool/si3/DAO/MentorDao.java
@@ -0,0 +1,64 @@
+package com.codecool.si3.DAO;
+
+import com.codecool.si3.model.Mentor;
+import com.codecool.si3.model.Entry;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MentorDAO  extends SQLDao<Mentor> implements Dao<Mentor> {
+
+    public MentorDAO(){
+        super("applicants", new String[]{"id", "first_name", "last_name", "nick_name", "phone_number", "email", "city", "favourite_number"});
+    }
+
+    @Override
+    protected Entry[] objectToArray(Mentor mentor) {
+        Entry id = new Entry("id", Integer.toString(mentor.getId()));
+        Entry first_name = new Entry("first_name", mentor.getFirst_name());
+        Entry last_name = new Entry("last_name", mentor.getLast_name());
+        Entry nick_name = new Entry("nick_name", mentor.getNick_name());
+        Entry phone_number = new Entry("pohne_number", mentor.getPhone_number());
+        Entry email = new Entry("email", mentor.getEmail());
+        Entry city = new Entry("city", mentor.getCity());
+        Entry favourite_number = new Entry("favourite_number", Integer.toString(mentor.getFavourite_number()));
+        return new Entry[]{id, first_name, last_name, nick_name, phone_number, email, city, favourite_number};
+    }
+
+    @Override
+    public void update(Mentor mentor) {
+        updateRecord(objectToArray(mentor));
+    }
+
+    @Override
+    public void remove(Mentor mentor) {
+        removeRecord(new Entry("id", Integer.toString(mentor.getId()))); }
+
+    @Override
+    public void insert(Mentor mentor) { insertRecord(objectToArray(mentor)); }
+
+    @Override
+    public List<Mentor> getObjects(Entry entry) {
+        List<Mentor> mentors = new ArrayList<>();
+        ResultSet resultSet = getRecords(entry);
+        try {
+            while (resultSet.next()) {
+                Mentor mentor = new Mentor();
+                mentor.setId(resultSet.getInt("id"));
+                mentor.setFirst_name(resultSet.getString("first_name"));
+                mentor.setLast_name(resultSet.getString("last_name"));
+                mentor.setNick_name(resultSet.getString("nick_name"));
+                mentor.setPhone_number(resultSet.getString("phone_number"));
+                mentor.setEmail(resultSet.getString("email"));
+                mentor.setCity(resultSet.getString("city"));
+                mentor.setFavourite_number(resultSet.getInt("favourite_nubmer"));
+                mentors.add(mentor);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return mentors;
+    }
+}
